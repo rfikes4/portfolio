5e6b8a70cef83542f65baa23e2cc4ea786468199
rfikes4 <robertfikesiv@RobertFikesIV.local>
2017-08-06 21:25:28 -0600

    Adding dist folder

--- dist/server.js
+++ dist/server.js
@@ -1,31 +1,28 @@
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// app = express();
-// app.use(serveStatic(__dirname));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
-
-
-
-
-const express = require('express');
-const history = require('connect-history-api-fallback');
-const app = express();
-
-const staticFileMiddleware = express.static(__dirname);
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
-const port = process.env.PORT || 5000;
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
-//const port = 5555;
+
+
+
+
+// const express = require('express');
+// const history = require('connect-history-api-fallback');
+// const app = express();
+
+// const staticFileMiddleware = express.static(__dirname);
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// app.use(staticFileMiddleware);
+
+// const port = 5555;
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${5555}!`);
 // });
